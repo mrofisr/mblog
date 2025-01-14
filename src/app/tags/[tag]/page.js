601dc77d0d1d70fb2a05c852c22e24a71c7953c5
Abdur Rofi <31870041+mrofisr@users.noreply.github.com>
2025-01-14 19:37:42 +0700

    update: slug tag page

---
+++ src/app/tags/[tag]/page.js
@@ -0,0 +1,51 @@
+import Layout from "@/components/custom/layout"
+import ListLayout from "@/components/custom/list-layout"
+import Title from "@/components/custom/title"
+import config from "@/config/config"
+import { getAllFilesFrontMatter } from "@/lib/mdx"
+
+const POSTS_PER_PAGE = 10
+
+export const metadata = {
+    title: config.page.blog.title,
+    description: config.page.blog.subtitle,
+}
+
+async function getBlogPosts() {
+    const params = useParams()
+    const posts = await getAllFilesFrontMatter("posts")
+    const filteredPosts = posts.filter(
+        (post) =>
+            post.draft !== true &&
+            post.tags.map((t) => kebabCase(t)).includes(params.tag)
+    );
+    const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
+    const pagination = {
+        currentPage: 1,
+        totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+    }
+
+    return {
+        posts,
+        initialDisplayPosts,
+        pagination,
+    }
+}
+
+export default async function TagPage() {
+    const { posts, initialDisplayPosts, pagination } = await getBlogPosts()
+    return (
+        <Layout>
+            <Title
+                title={config.page.blog.title}
+                subtitle={config.page.blog.subtitle}
+            />
+            <ListLayout
+                posts={posts}
+                initialDisplayPosts={initialDisplayPosts}
+                pagination={pagination}
+                title="All Posts"
+            />
+        </Layout>
+    )
+}
