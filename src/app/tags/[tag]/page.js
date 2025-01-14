090ed86238f391c7bc7cb838473e2b1e27b4f31f
Abdur Rofi <31870041+mrofisr@users.noreply.github.com>
2025-01-14 19:43:47 +0700

    update: pages tags slug

--- src/app/tags/[tag]/page.js
+++ src/app/tags/[tag]/page.js
@@ -3,6 +3,8 @@ import ListLayout from "@/components/custom/list-layout"
 import Title from "@/components/custom/title"
 import config from "@/config/config"
 import { getAllFilesFrontMatter } from "@/lib/mdx"
+import kebabCase from "@/lib/kebab-case"
+import formatDate from "@/lib/format-date"
 
 const POSTS_PER_PAGE = 10
 
@@ -11,29 +13,30 @@ export const metadata = {
     description: config.page.blog.subtitle,
 }
 
-async function getBlogPosts() {
-    const params = useParams()
+async function getBlogPosts(tag) {
     const posts = await getAllFilesFrontMatter("posts")
     const filteredPosts = posts.filter(
         (post) =>
             post.draft !== true &&
-            post.tags.map((t) => kebabCase(t)).includes(params.tag)
-    );
-    const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
+            post.tags.map((t) => kebabCase(t)).includes(tag)
+    )
+    const initialDisplayPosts = filteredPosts.slice(0, POSTS_PER_PAGE)
     const pagination = {
         currentPage: 1,
-        totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+        totalPages: Math.ceil(filteredPosts.length / POSTS_PER_PAGE),
     }
 
     return {
-        posts,
+        posts: filteredPosts,
         initialDisplayPosts,
         pagination,
     }
 }
 
-export default async function TagPage() {
-    const { posts, initialDisplayPosts, pagination } = await getBlogPosts()
+export default async function TagPage({ params }) {
+    const tag = params.tag
+    const { posts, initialDisplayPosts, pagination } = await getBlogPosts(tag)
+
     return (
         <Layout>
             <Title
