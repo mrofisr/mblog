c0d824872eb050e84fa8bd91e65f673c896b14a9
Abdur Rofi <31870041+mrofisr@users.noreply.github.com>
2025-01-14 20:24:13 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -50,6 +50,19 @@
 // export default withMDX(nextConfig)
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    // Important: return the modified config
+    return config
+  },
+}
+ 
+export default nextConfig
